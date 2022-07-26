6c58890f0cfad47c46e39113d74434e0ea0bc2ec
Taylor Fort <tfort@riotgames.com>
2022-07-26 16:49:33 -0700

    feat: use the .env file instead of hard coding paths

--- pull_users.sh
+++ pull_users.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+if [ -f .env ]
+then
+  export $(cat .env | grep "^[^#;]" | xargs) >/dev/null
+fi
+
 for user in "$@"
 do
   node user-commit-history-report.js --user $user
 done
 
-awk '(NR == 1) || (FNR > 1)' /tmp/commit-history/*.csv > report.csv
-rm /tmp/commit-history/*.csv
-
+awk '(NR == 1) || (FNR > 1)' ${OUTPUT_FOLDER}/*.csv > report.csv
+rm ${OUTPUT_FOLDER}/*.csv
